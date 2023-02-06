bfa64df5dfefdeb890d498be0d71eaed8ebb5659
shoakbar-sh <shoakbarshonosirov>
2023-02-06 18:18:16 +0500

    card

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict"
+
+
+let doc = document.querySelector("body")
+
+doc.style.cssText = "display:flex; gap:150px; width: 1518px; margin:auto; padding:auto; padding-top:50px; background-image: url(https://picsum.photos/id/1/340/250); background-repeat:no-repeat; background-size:cover;";
+
+
+let box = document.querySelector('.card'),
+   soz = document.querySelector('.num'),
+   put = document.querySelector('.gap');
+
+box.style.cssText = "background-color:silver; text-align:center; width: 350px; height:500px; padding:15px; overflow: auto; margin-left:40px;"
+soz.style.cssText = "font-size:30px;"
+put.style.cssText = "padding:10px;"
+
+console.log(card.style);
+
+
+
+let quti = document.querySelector('.quti'),
+   raq = document.querySelector('.raq'),
+   til = document.querySelector('.til');
+
+quti.style.cssText = "background-color:gold; text-align:center; width: 350px; height:500px; padding:15px; overflow: auto;"
+raq.style.cssText = "font-size:30px;"
+til.style.cssText = "padding:10px;"
+
+console.log(quti.style);
+
+
+
+let late = document.querySelector('.late'),
+   number = document.querySelector('.number'),
+   gapla = document.querySelector('.gapla');
+
+late.style.cssText = "background-color:grey; color:white; text-align:center; width: 350px; height:500px; padding:15px; overflow: auto;"
+number.style.cssText = "font-size:30px;"
+gapla.style.cssText = "padding:10px;"
+
+console.log(box.late);
